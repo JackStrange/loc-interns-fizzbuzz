0f3063f744df9ac80adecccf058b9309ff8cb757
JackStrange <jack.d.strange.business@gmail.com>
2023-07-03 17:06:48 +0100

    Part 2 - FizzBuzzBangFezzEtcEtc

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -9,6 +9,36 @@ function fizzbuzz() {
     }
 }
 
+// Part 2
+function fizzbuzzplus() {
+    for(var i = 1; i <= 1000; i++) {
+        let out_arr = [];
+        if(i%11 !== 0){
+            if(i%3 === 0) out_arr.push("Fizz");
+            if(i%5 === 0) out_arr.push("Buzz");
+            if(i%7 === 0) out_arr.push("Bang");
+        }else{
+            out_arr.push("Bong");
+        }
+        if(i%13 === 0) {
+            let add_flag = false;
+            let j = 0;
+            while(!add_flag && j < out_arr.length){
+                if(out_arr[j][0] === 'B'){
+                    out_arr.splice(j,0,"Fezz");
+                    add_flag = true;
+                }
+                j++;
+            }
+            if(!add_flag) out_arr.push("Fezz");
+        }
+        if(i%17 === 0) out_arr = out_arr.reverse();
+
+        if(out_arr.length === 0) console.log(i);
+        else console.log(out_arr.join(""));
+    }
+}
+
 // Now, we run the main function:
-fizzbuzz();
+fizzbuzzplus();
 
