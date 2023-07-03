33239661b617c37aeabba1373d9283d26eb61ece
JackStrange <jack.d.strange.business@gmail.com>
2023-07-03 17:01:12 +0100

    Part 1 - FizzBuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,9 +1,12 @@
-// This is our main function
+// Part 1
 function fizzbuzz() {
-    console.log("Hello, World!");
-
-    // Put your code here...
-
+    for(var i = 1; i <= 100; i++) {
+        let output = ""
+        if(i%3 === 0) output += "Fizz";
+        if(i%5 === 0) output += "Buzz";
+        if(output === "") console.log(i);
+        else console.log(output);
+    }
 }
 
 // Now, we run the main function:
